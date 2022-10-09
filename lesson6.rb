829f3156ba9b969c7561882c098b4d567a2285da
EC2 Default User <ec2-user@ip-172-31-47-39.ap-northeast-1.compute.internal>
2022-10-09 08:58:57 +0000

    (finish)ruby_2-7

--- lesson6.rb
+++ lesson6.rb
@@ -4,22 +4,22 @@ puts "何回計算を繰り返しますか？"
 input_times = gets.to_i
 
 i = 1
-while i <= input_times do 
-  
+while i <= input_times do
+
   puts "#{i}回目の計算"
   puts "2つの値を入力してください"
     a = gets.to_i
     b = gets.to_i
     puts "a=#{a}"
     puts "b=#{b}"
-    
+
     puts "計算結果を出力します"
     puts "#{a}+#{b}=#{a + b}"
     puts "#{a}-#{b}=#{a - b}"
     puts "#{a}*#{b}=#{a * b}"
     puts "#{a}/#{b}=#{a / b}"
-  
+
   i += 1
 end
-  
+
 puts "計算結果を終了します"
